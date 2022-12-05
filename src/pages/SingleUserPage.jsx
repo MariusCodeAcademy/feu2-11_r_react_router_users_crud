13ec38bd0455ee8f8dff13ca90c39bfe9931a22d
Marius <marius@example.com>
2022-12-05 10:53:28 +0200

    get id of current user

--- src/pages/SingleUserPage.jsx
+++ src/pages/SingleUserPage.jsx
@@ -1,9 +1,18 @@
+import { useParams } from 'react-router-dom';
 import { Section, Title } from '../components/styled/UI.styled';
 
 function SingleUserPage(props) {
+  const allParams = useParams();
+  const currentUserId = allParams.userId;
+
+  // susikurti data state for current user currentUser
+  // parsisiusti su feth useEffecte duomenis apie konkretu useri
+  // is gautu duomenu atvaizduojam daugiau info apie useri
+
   return (
     <Section>
       <Title>SingleUserPage</Title>
+      <p>I should load 🦸 with id: {currentUserId}</p>
     </Section>
   );
 }
