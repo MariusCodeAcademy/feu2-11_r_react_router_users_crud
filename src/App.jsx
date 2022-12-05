c288f0bd8fcb8eb1bd65186d2dd02559f642a7ce
Marius <marius@example.com>
2022-12-05 10:36:05 +0200

    static user route and link

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import UsersPage from './pages/UsersPage';
+import SingleUserPage from './pages/SingleUserPage';
 
 function App() {
   return (
     <div className='App'>
+      {/* prideti route /users/2 gaunam SingleUserPage */}
       <Switch>
+        <Route path={'/users/2'}>
+          <SingleUserPage />
+        </Route>
         <Route path={'/users'}>
           <UsersPage />
         </Route>
