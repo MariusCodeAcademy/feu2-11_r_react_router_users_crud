b54506c1b77c08d16113cb3b96383055078fe34f
Marius <marius@example.com>
2022-12-05 11:30:48 +0200

    nav css links

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import UsersPage from './pages/UsersPage';
 import SingleUserPage from './pages/SingleUserPage';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       {/* prideti route /users/2 gaunam SingleUserPage */}
       <Switch>
         {/* :userId dinaminis parametras kurio reiksme galime pasiimti is useParams() hook */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,45 @@
+import { Container } from '../styled/UI.styled';
+import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderEl = styled.header`
+  background-color: #333;
+  color: snow;
+`;
+// prideti esamam Cotainer papildomu savybiu css
+const ThisContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Nav = styled.nav``;
+
+const LinkTo = styled(Link)`
+  display: inline-block;
+  padding: 0.4em 1em;
+  color: inherit;
+  font-size: 1.1rem;
+`;
+
+const Logo = styled(Link)`
+  padding: 0.4em 1em;
+  color: inherit;
+  font-size: 1.5rem;
+  padding-left: 0;
+`;
+
 function Header(props) {
   return (
-    <header>
-      <nav>
-        {/* sukurti nuorodas i Home ir Users */}
-        {/* pastylinti su styled components */}
-        nuorodos
-      </nav>
-    </header>
+    <HeaderEl>
+      <ThisContainer>
+        <Logo>ReactRouter</Logo>
+        <Nav>
+          <LinkTo to='/'>Home</LinkTo>
+          <LinkTo to='/users'>Users</LinkTo>
+        </Nav>
+      </ThisContainer>
+    </HeaderEl>
   );
 }
 export default Header;
