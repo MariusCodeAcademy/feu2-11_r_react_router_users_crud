aabb4d67188751a5a882598a964b073297b0c066
Marius <marius@example.com>
2022-12-05 09:15:47 +0200

    uzd

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,6 +1,9 @@
 import { Section } from '../components/styled/UI.styled';
 
 function UsersPage(props) {
+  // susikurti usersArr state
+  // su useEffect supildyti state su duomenimis is https://dummyjson.com/users
+
   return (
     <Section>
       <h1>UsersPage</h1>
