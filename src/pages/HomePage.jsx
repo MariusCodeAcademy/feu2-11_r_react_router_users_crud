06d77b603fa1d9eac6867d4a4004fbc5014b7ab1
Marius <marius@example.com>
2022-12-05 08:34:50 +0200

    init

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage(props) {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>Welcome welcome</p>
+    </div>
+  );
+}
+export default HomePage;
---
+++ src/pages/UsersPage.jsx
@@ -0,0 +1,9 @@
+function UsersPage(props) {
+  return (
+    <div>
+      <h1>UsersPage</h1>
+      <p>This is our users</p>
+    </div>
+  );
+}
+export default UsersPage;
