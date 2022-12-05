d9bd450f7223bafc764cdc7263f7c4f8d3b1eac8
Marius <marius@example.com>
2022-12-05 10:58:36 +0200

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+function Header(props) {
+  return (
+    <header>
+      <nav>
+        {/* sukurti nuorodas i Home ir Users */}
+        {/* pastylinti su styled components */}
+        nuorodos
+      </nav>
+    </header>
+  );
+}
+export default Header;
