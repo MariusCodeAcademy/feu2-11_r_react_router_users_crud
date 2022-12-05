a96db06ae68f73f0af9971823a92bdff0806af1a
Marius <marius@example.com>
2022-12-05 09:50:09 +0200

    local data

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from 'react';
 import { Section } from '../components/styled/UI.styled';
 
 function UsersPage(props) {
   // susikurti usersArr state
+  const [usersArr, setUsersArr] = useState([]);
   // su useEffect supildyti state su duomenimis is https://dummyjson.com/users
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      let url = 'https://dummyjson.com/users';
+      url = '/api/users.json';
+      const resp = await fetch(url);
+      const dataInJs = await resp.json();
+      // const usersWithNameAndId = dataInJs.users.map((uOb) => ({
+      //   id: uOb.id,
+      //   firstName: uOb.firstName,
+      //   lastName: uOb.lastName,
+      //   age: uOb.age,
+      // }));
+      // console.log(JSON.stringify(usersWithNameAndId));
+
+      // dataInJs === {users: Array(30), total: 100, skip: 0, limit: 30}
+      console.log('dataInJs ===', dataInJs.users);
+    } catch (error) {
+      console.warn('klaida getData', error);
+    }
+  }
 
   return (
     <Section>
